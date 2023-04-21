de54806bc9f6ba335cdbb1ca3ade65b4f6e4f656
Mateus Borges <spekytro68@gmail.com>
2023-04-21 19:02:42 -0300

    ajustes

--- front-end/src/service/service.ts
+++ front-end/src/service/service.ts
@@ -8,15 +8,18 @@ interface Result {
 const CreateMessage = async (prompt: string): Promise<any> => {
   const { getApi } = useApi();
 
-  const result: Result = await getApi(process.env.URL_API + "/gpt", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: prompt,
-    }),
-  });
+  const result: Result = await getApi(
+    "https://api-generaitor-phrase-gpt.onrender.com/gpt",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+      }),
+    }
+  );
 
   return result.response;
 };
